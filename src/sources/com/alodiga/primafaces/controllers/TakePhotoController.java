48d9c0d3188fd642a3c1d653182c10458bc6fcf8
yamealm <yamealm@gmail.com>
2020-03-25 18:21:16 -0300

    Cambios para guardar solicitud de tarjetas. Actualizar ejb para guardar tarjetas

--- src/sources/com/alodiga/primafaces/controllers/TakePhotoController.java
+++ src/sources/com/alodiga/primafaces/controllers/TakePhotoController.java
@@ -15,7 +15,8 @@
  */
 package com.alodiga.primafaces.controllers;
 
-import com.alodiga.cms.commons.ejb.PersonEJB;
+import com.alodiga.cms.commons.ejb.ProductEJB;
+import com.alodiga.cms.commons.ejb.ProgramEJB;
 import com.alodiga.cms.commons.ejb.RequestEJB;
 import com.alodiga.cms.commons.ejb.UtilsEJB;
 import com.alodiga.cms.commons.exception.EmptyListException;
@@ -27,15 +28,21 @@ import com.cms.commons.util.EJBServiceLocator;
 import com.cms.commons.util.EjbConstants;
 import com.cms.commons.genericEJB.EJBRequest;
 import com.cms.commons.models.ApplicantNaturalPerson;
-import com.cms.commons.models.ImagensAplicantNaturalPerson;
+import com.cms.commons.models.CollectionType;
+import com.cms.commons.models.CollectionsRequest;
+import com.cms.commons.models.PersonType;
+import com.cms.commons.models.ProductType;
+import com.cms.commons.models.Program;
+import com.cms.commons.models.RequestHasCollectionsRequest;
+import com.cms.commons.util.Constants;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -46,7 +53,6 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.servlet.ServletContext;
 import org.primefaces.context.RequestContext;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.UploadedFile;
@@ -57,8 +63,9 @@ import org.primefaces.ultima.domain.Solicitude;
 public class TakePhotoController {
     public Solicitude solicitude;
     private UtilsEJB utilsEJB;
-    private PersonEJB personEJB;
     private RequestEJB requestEJB;
+    private ProductEJB productEJB;
+    private ProgramEJB programEJB;
     private String cellNumber;
     private Country country;
     private Map<String, String> countries = null;
@@ -69,11 +76,12 @@ public class TakePhotoController {
     public void init() {
         try {
             utilsEJB = (UtilsEJB) EJBServiceLocator.getInstance().get(EjbConstants.UTILS_EJB);
-            personEJB = (PersonEJB) EJBServiceLocator.getInstance().get(EjbConstants.PERSON_EJB);
-            requestEJB= (RequestEJB) EJBServiceLocator.getInstance().get(EjbConstants.REQUEST_EJB);
-            EJBRequest request = new EJBRequest();
-            request.setParam(2);
-            country = utilsEJB.loadCountry(request);
+            requestEJB = (RequestEJB) EJBServiceLocator.getInstance().get(EjbConstants.REQUEST_EJB);
+            productEJB  = (ProductEJB) EJBServiceLocator.getInstance().get(EjbConstants.PRODUCT_EJB);
+            programEJB = (ProgramEJB) EJBServiceLocator.getInstance().get(EjbConstants.PROGRAM_EJB);
+            EJBRequest request1 = new EJBRequest();
+            request1.setParam(2);
+            country = utilsEJB.loadCountry(request1);
             applicantNaturalPerson = (ApplicantNaturalPerson) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("applicantNaturalPerson");
         } catch (RegisterNotFoundException ex) {
             Logger.getLogger(TakePhotoController.class.getName()).log(Level.SEVERE, null, ex);
@@ -111,7 +119,7 @@ public class TakePhotoController {
             SimpleDateFormat fmt = new SimpleDateFormat("yyyyMMddHHmmss");
             String name = fmt.format(new Date()) + event.getFile().getFileName().substring( event.getFile().getFileName().lastIndexOf('.'));
             String id =applicantNaturalPerson!=null?applicantNaturalPerson.getId().toString():"0";
-            File file = new File("C:\\Users\\yalmea\\Documents\\NetBeansProjects\\upload\\document-" + id + "-"+name);
+            File file = new File("C:\\Users\\yamea\\OneDrive\\Documentos\\NetBeansProjects\\upload\\document-" + id + "-"+name);
 //          File file = new File("home/" + requestPersonId + "-"+name);
 
             InputStream is = event.getFile().getInputstream();
@@ -123,13 +131,69 @@ public class TakePhotoController {
             }
             is.close();
             out.close();
+            short indApproved = 1;
+            EJBRequest request1 = null;
+            Map params = null;
+            PersonType personTypeApp = null;
+            CollectionsRequest collectionsRequest = null;
             try {
-                ImagensAplicantNaturalPerson imagen = new ImagensAplicantNaturalPerson();
-                imagen.setApplicantNaturalPersonId(applicantNaturalPerson);
-                imagen.setObservations("Imagen del documento de ApplicantNaturalPersonId: " + applicantNaturalPerson.getId());
-                imagen.setUrlImageFile(file.getAbsolutePath());
-                imagen.setCreateDate(new Timestamp(new Date().getTime()));
-                requestEJB.saveImagensAplicantNaturalPerson(imagen);
+                List<CollectionsRequest> collectionsRequests = null;
+
+                request1 = new EJBRequest();
+                params = new HashMap();
+                params.put(Constants.COUNTRY_KEY, country.getId());
+                params.put(Constants.ORIGIN_APPLICATION_KEY, Constants.ORIGIN_APPLICATION_WALLET_ID);
+                request1.setParams(params);
+                List<PersonType> personTypes = utilsEJB.getPersonTypeByCountry(request1);
+                for (PersonType p : personTypes) {
+                    if (p.getOriginApplicationId().getId() == Constants.ORIGIN_APPLICATION_WALLET_ID) {
+                        personTypeApp = p;
+                    }
+                }
+
+                params = new HashMap();
+                request1 = new EJBRequest();
+                params.put(Constants.COUNTRY_KEY, country.getId());
+                params.put(Constants.PRODUCT_TYPE_KEY, Constants.PRODUCT_TYPE_WALLET_APP_ID);
+                params.put(Constants.PROGRAM_KEY, Constants.PROGRAM_WALLET_APP_ID);
+                params.put(Constants.PERSON_TYPE_KEY, personTypeApp.getId());
+                request1.setParams(params);
+                try {
+                    collectionsRequests = requestEJB.getCollectionsByRequest(request1);
+                    for (CollectionsRequest collectionsRequest1 : collectionsRequests) {
+                        if (collectionsRequest1.getId() == Constants.APP_IDENTIFICATION_DOCUMENT) {
+                            collectionsRequest = collectionsRequest1;
+                        }
+                    }
+                } catch (EmptyListException e) {
+                    request1 = new EJBRequest();
+                    request1.setParam(Constants.PROGRAM_WALLET_APP_ID);
+                    Program program = programEJB.loadProgram(request1);
+                    request1 = new EJBRequest();
+                    request1.setParam(Constants.PRODUCT_TYPE_WALLET_APP_ID);
+                    ProductType productType = productEJB.loadProductType(request1);
+                    request1 = new EJBRequest();
+                    request1.setParam(Constants.APP_IDENTIFICATION_DOCUMENT);
+                    CollectionType collestionType = requestEJB.loadCollectionType(request1);
+                    collectionsRequest = new CollectionsRequest();
+                    collectionsRequest.setCountryId(country);
+                    collectionsRequest.setProductTypeId(productType);
+                    collectionsRequest.setProgramId(program);
+                    collectionsRequest.setPersonTypeId(personTypeApp);
+                    collectionsRequest.setCollectionTypeId(collestionType);
+                    collectionsRequest = requestEJB.saveCollectionRequest(collectionsRequest);
+
+                }
+
+                RequestHasCollectionsRequest requestHasCollectionsRequest = new RequestHasCollectionsRequest();
+
+                requestHasCollectionsRequest.setCollectionsRequestid(collectionsRequest);
+                requestHasCollectionsRequest.setRequestId(applicantNaturalPerson.getRequest());
+                requestHasCollectionsRequest.setIndApproved(indApproved);
+                requestHasCollectionsRequest.setObservations("Imagen del documento de ApplicantNaturalPersonId: " + applicantNaturalPerson.getId());
+                requestHasCollectionsRequest.setUrlImageFile(file.getAbsolutePath());
+                requestHasCollectionsRequest.setCreateDate(new Timestamp(new Date().getTime()));
+                requestHasCollectionsRequest = requestEJB.saveRequestHasCollectionsRequest(requestHasCollectionsRequest);
                 FacesContext.getCurrentInstance().getExternalContext().redirect("sendPhoto.xhtml");
             } catch (NullParameterException ex) {
                 Logger.getLogger(SendPhotoController.class.getName()).log(Level.SEVERE, null, ex);
