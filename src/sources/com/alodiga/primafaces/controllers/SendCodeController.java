efa50e86e4ea6a57e8ec6c4a745182fe5cec3f6d
Yamelis <yamealm@gmail.com>
2020-11-05 11:40:28 -0300

    incidencias de las solicitud de tarjetas ajustes para desarrollo

--- src/sources/com/alodiga/primafaces/controllers/SendCodeController.java
+++ src/sources/com/alodiga/primafaces/controllers/SendCodeController.java
@@ -156,18 +156,18 @@ public class SendCodeController {
             FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("cellNumber", cellNumber);
             FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("codeCellNumber", codeCellNumber);
             FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("country", country);
-//            APIRegistroUnificadoProxy proxy = new APIRegistroUnificadoProxy();
-//            
-//            RespuestaCodigoRandom response =proxy.generarCodigoMovilSMS("usuarioWS","passwordWS",codeCellNumber+cellNumber);
-//            System.out.println("Respuesta Code:"+response.getCodigoRespuesta());
-//            System.out.println("Respuesta Mensaje:"+response.getMensajeRespuesta());
-//            System.out.println("Respuesta Codigo:"+response.getDatosRespuesta());
-//            if (response.getCodigoRespuesta().equals("00")) {
-//                FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("codigo", response.getDatosRespuesta());
-//                FacesContext.getCurrentInstance().getExternalContext().redirect("validateCode.xhtml");
-           if (true) {
-                FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("codigo", "12345");
+            APIRegistroUnificadoProxy proxy = new APIRegistroUnificadoProxy();
+            
+            RespuestaCodigoRandom response =proxy.generarCodigoMovilSMS("usuarioWS","passwordWS",codeCellNumber+cellNumber);
+            System.out.println("Respuesta Code:"+response.getCodigoRespuesta());
+            System.out.println("Respuesta Mensaje:"+response.getMensajeRespuesta());
+            System.out.println("Respuesta Codigo:"+response.getDatosRespuesta());
+            if (response.getCodigoRespuesta().equals("00")) {
+                FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("codigo", response.getDatosRespuesta());
                 FacesContext.getCurrentInstance().getExternalContext().redirect("validateCode.xhtml");
+//           if (true) {
+//                FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("codigo", "12345");
+//                FacesContext.getCurrentInstance().getExternalContext().redirect("validateCode.xhtml");
             }else{
                 messages = bundle.getString("common.error.send.code");
                 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(messages)); 
